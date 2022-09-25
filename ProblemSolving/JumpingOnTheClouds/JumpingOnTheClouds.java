9c279f27c0c3c84637a14e49bf3a5406aef9ff62
e-hto <s1710016@ed.ac.uk>
2022-09-25 13:03:43 +0100

    Nearly there

--- ProblemSolving/JumpingOnTheClouds/JumpingOnTheClouds.java
+++ ProblemSolving/JumpingOnTheClouds/JumpingOnTheClouds.java
@@ -9,7 +9,7 @@ public class JumpingOnTheClouds{
         // 0 : safe (cumulus)
         // 1 : avoid (thunderhead)
         
-        // Player can jump by own or two  
+        // Player can jump by one or two  
         // clouds at a time
         
         // Return no. of jumps to reach end
@@ -20,42 +20,72 @@ public class JumpingOnTheClouds{
         
         for (int i = 1; i < c.size(); i ++) {
             
-            // Cumulus
-            if (isCumulus(c,i)) {
-        		
+			if(jump(c,i,position)){
         		// jump!
         		jumps ++;
         		position = i;
-        	
-        	}	    
+        		}
 
-            // Thunderhead
-            else {
-
-            }
         }
 
         return jumps;
     }
 
-    public static boolean isCumulus(List<Integer> c, int i){
+    public static boolean isCumulus(List<Integer> c, int i) {
 
     	return c.get(i) == 0 ? true : false;
 
     }
 
+    public static boolean jump(List<Integer> c, int i, int position) {
+
+    	// Given cloud and current position,
+    	// looks ahead to decide
+    	// whether to jump onto this cloud
+
+    	// On a cumulus, not the last
+    	if (isCumulus(c,i) && i < c.size()-1) {
+    		
+    		// If the next cloud is a thunderhead
+    		if(!isCumulus(c,i+1)) {
+    			return true;
+    		}
+ 
+ 			// If the next cloud is a cumulus
+    		else if (position == i - 2) { 
+    			return true;
+    		}
+    	}
+
+    	return false;
+
+    }
+
     public static void main(String[] args) {
 
+    	// [0,1,0,0,0,1,0]
     	ArrayList<Integer> c = new ArrayList<Integer>();
     	c.add(0);
-    	c.add(0);
     	c.add(1);
+    	c.add(0);
     	c.add(0);
     	c.add(0);
     	c.add(1);
     	c.add(0);
 
     	System.out.println("output: " + jumpingOnClouds(c));
+
+    	// [0,0,1,0,0,1,0]
+    	ArrayList<Integer> c2 = new ArrayList<Integer>();
+    	c2.add(0);
+    	c2.add(0);
+    	c2.add(1);
+    	c2.add(0);
+    	c2.add(0);
+    	c2.add(1);
+    	c2.add(0);
+
+    	System.out.println("output: " + jumpingOnClouds(c2));
     }
 
 }
