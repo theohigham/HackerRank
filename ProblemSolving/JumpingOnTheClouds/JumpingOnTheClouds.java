9370e3a8aec69a58622c56eb46ccdab04fd2a8c7
e-hto <s1710016@ed.ac.uk>
2022-09-25 13:41:53 +0100

    New version

--- ProblemSolving/JumpingOnTheClouds/JumpingOnTheClouds.java
+++ ProblemSolving/JumpingOnTheClouds/JumpingOnTheClouds.java
@@ -14,60 +14,42 @@ public class JumpingOnTheClouds{
         
         // Return no. of jumps to reach end
         
-        int position = 0;
         int jumps = 0;
-        int gap = 0;
+        boolean justJumped = false; // Whether the player jumped at the previous cloud
         
         for (int i = 1; i < c.size(); i ++) {
-            
-			if(jump(c,i,position)){
+        	int cloud = c.get(i);
+
+			if(jump(cloud, justJumped)){
         		// jump!
         		jumps ++;
-        		position = i;
-        		}
+        	}
+
+        	justJumped = jump(cloud,justJumped);
 
         }
 
         return jumps;
     }
 
-    public static boolean isCumulus(List<Integer> c, int i) {
+    public static boolean isCumulus(int c) {
 
-    	return c.get(i) == 0 ? true : false;
+    	return c == 0 ? true : false;
 
     }
 
-    public static boolean jump(List<Integer> c, int i, int position) {
+    public static boolean jump(int cloud, boolean justJumped) {
 
-    	// Given cloud and current position,
-    	// looks ahead to decide
-    	// whether to jump onto this cloud
-
-    	// On a cumulus, not the last
-    	if (isCumulus(c,i) && i < c.size()-1) {
-    		
-    		// If the next cloud is a thunderhead
-    		if(!isCumulus(c,i+1)) {
-    			return true;
-    		}
- 
- 			// If the next cloud is a cumulus
-    		else if (position == i - 2) { 
-    			return true;
-    		}
-    	}
-
-    	return false;
+    	return cloud == 0 && justJumped == false;
 
     }
 
     public static void main(String[] args) {
 
-    	// [0,1,0,0,0,1,0]
+    	// [0,1,0,0,1,0]
     	ArrayList<Integer> c = new ArrayList<Integer>();
     	c.add(0);
     	c.add(1);
-    	c.add(0);
     	c.add(0);
     	c.add(0);
     	c.add(1);
@@ -75,17 +57,6 @@ public class JumpingOnTheClouds{
 
     	System.out.println("output: " + jumpingOnClouds(c));
 
-    	// [0,0,1,0,0,1,0]
-    	ArrayList<Integer> c2 = new ArrayList<Integer>();
-    	c2.add(0);
-    	c2.add(0);
-    	c2.add(1);
-    	c2.add(0);
-    	c2.add(0);
-    	c2.add(1);
-    	c2.add(0);
-
-    	System.out.println("output: " + jumpingOnClouds(c2));
     }
 
 }
